6fd09add776523fbca48940034ceb0b58fb09329
Traniks <andert2017@gmail.com>
2024-11-15 12:56:08 +0300

    feat: new mail.php

--- public/mail.php
+++ public/mail.php
@@ -1,24 +1,20 @@
 <?php
-// include('amocrm/lead.php');
-include('amocrm/lead.php');
+$to = "sales@relocant.ae"; // email получателя
+$tema = "Заявка"; // тема письма
 
-$to = "sales@relocant.ae"; // емайл получателя данных из формы
-$tema = "Заявка"; // тема полученного емайла
-
-$message .= "Имя: " . $_POST['username'] . "<br>"; // полученное из формы name=username
+$message = "Имя: " . $_POST['username'] . "<br>"; // полученное из формы name=username
 $message .= "Телефон: " . $_POST['usertel'] . "<br>"; // полученное из формы name=usertel
 
-// $message .= "Соц.Сеть: " . $_POST['type'] . "<br>";
-// $message .= "Соц.Сеть: " . $_POST['type'] . "<br>";
-// $message .= "Время: " . $_POST['date'] . "<br>";
+$headers = 'MIME-Version: 1.0' . "\r\n";
+$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n";
 
-$headers = 'MIME-Version: 1.0' . "\r\n"; // заголовок соответствует формату плюс символ перевода строки
-$headers .= 'Content-type: text/html; charset=utf-8' . "\r\n"; // указывает на тип посылаемого контента
-
-mail($to, $tema, $message, $headers); // отправляет получателю на емайл значения переменных
-header('Location: /');
-if (isset($_POST['submitform']))
+if (mail($to, $tema, $message, $headers)) {
     
+    echo json_encode(['success' => true]);
+} else {
+    
+    echo json_encode(['success' => false, 'message' => 'Ошибка при отправке почты.']);
+}
+
+exit; 
 ?>
-<script type="text/javascript">
-</script>   
